f759c141506b655de66a025e73f43238a89c1bb4
screamman-ssh <autogran41@gmail.com>
2024-04-13 23:07:03 +0700

    add modal animation

--- front/app/(pages)/subjectSearch/subjectCard.tsx
+++ front/app/(pages)/subjectSearch/subjectCard.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react"
 import SectionCard from "./sectionCard"
 import facultyData from '../../../public/faculty-data.json'
+import { AnimatePresence, motion } from "framer-motion"
 
 export default function SubjectCard(prop: any) {
     const data = prop.data
@@ -49,74 +50,76 @@ export default function SubjectCard(prop: any) {
         }
         setShowModal(!showModal)
     }
-    
+
     const faculty = facultyData.data.filter((fac) => fac.facId == data.facId)[0]
     const facColor = {
         "primary": faculty.primaryColor,
         "secondary": faculty.secondaryColor
     }
-    const [facultyColor, setFacultyColor] = useState({})
-    useEffect(()=>{
-        const faculty = facultyData.data.filter((fac) => fac.facId == data.facId)[0]
-        setFacultyColor({
-            "primary": faculty.primaryColor,
-            "secondary": faculty.secondaryColor
-        })
-    }, [])
+
     return (
         <>
-        {/* {console.log(facColor)} */}
+            {/* {console.log(facColor)} */}
             <a className="md:h-44 pl-2 border rounded-lg shadow hover:shadow-md hover:scale-[1.01] transition-all" style={{ background: `linear-gradient(to bottom, ${facColor.primary}, ${facColor.secondary})`, }} onClick={handleCardClick}>
                 <div className="p-4 h-full bg-white gap-1 rounded-r-md">
                     <p className="font-bold text-green-950">{subjectShortNameEN}</p>
-                    <p className="text-green-800 truncate max-h-6">{data.subjectNameThai}</p>
+                    <p className="text-gray-800 truncate max-h-6">{data.subjectNameThai}</p>
                     <p className="text-gray-500">{data.credit}</p>
                 </div>
             </a>
-            {showModal && (
-                <>
-                    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-[110] outline-none overscroll-auto">
-                        <div className="relative w-full my-6 mx-auto max-w-3xl">
-                            <div className="w-full grid grid-cols-1 gap-2 border-0 rounded-lg shadow-lg relative bg-white outline-none focus:outline-none">
-                                <div className="items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                                    <button
-                                        className="absolute top-1 right-0 px-2 text-gray-500 hover:bg-gray-300 hover:text-red-500 rounded-lg font-bold uppercase text-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                        type="button"
-                                        onClick={handleCardClick}
-                                    >
-                                        X
-                                    </button>
-                                    <p className="font-bold text-lg text-green-950">{subjectNameEN}</p>
-                                    <p className="text-green-800">{data.subjectNameThai}</p>
-                                    <p className="text-gray-500">{data.credit}</p>
-                                </div>
-                                <div className="relative px-5 flex-auto">
-                                    <p className="font-bold">รายละเอียด</p>
-                                    <p className="text-gray-900 text-sm leading-relaxed">
-                                        {/* {console.log(courseDetail)} */}
-                                        {courseDetail.subjectDescThai ? courseDetail.subjectDescThai : "ไม่มีข้อมูล"}
-                                    </p>
-                                </div>
-                                <div className="relative px-5 grid grid-cols-1 gap-2">
-                                    {console.log(sectionDate)}
-                                    <p className="font-bold">ตอน</p>
-                                    {courseSection ? courseSection.map((section, key) => <SectionCard key={key} data={section} />) : "ไม่มีข้อมูล"}
-                                </div>
-                                <div className="flex items-center justify-end p-5 border-t border-solid border-blueGray-200 rounded-b">
-                                    <button
-                                        className=" text-red-500 rounded-lg font-bold uppercase text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                        type="button"
-                                        onClick={handleCardClick}
-                                    >
-                                        Close
-                                    </button>
-                                </div>
+            <AnimatePresence>
+                {showModal && (
+                    <>
+                        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-[110] outline-none overscroll-auto">
+                            <div className="relative w-full my-6 mx-auto max-w-3xl">
+                                <motion.div
+                                    className="w-full h-fit grid grid-cols-1 gap-2 border-0 rounded-lg shadow-lg relative bg-white outline-none focus:outline-none"
+                                    initial={{ opacity: 0, scale: 0.75 }}
+                                    animate={{ opacity: 1, scale: 1, transition: { ease: "easeOut", duration: 0.10 } }}
+                                    exit={{ opacity: 0, scale: 0.75, transition: { ease: "easeIn", duration: 0.10 } }}
+                                >
+                                    <div className="items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                                        <button
+                                            className="absolute top-1 right-0 px-2 text-gray-500 hover:bg-gray-300 hover:text-red-500 rounded-lg font-bold uppercase text-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                            type="button"
+                                            onClick={handleCardClick}
+                                        >
+                                            X
+                                        </button>
+                                        <p className="font-bold text-lg text-green-950">{subjectNameEN}</p>
+                                        <p className="text-green-800">{data.subjectNameThai}</p>
+                                        <p className="text-gray-500">{data.credit}</p>
+                                    </div>
+                                    <div className="overflow-y-scroll">
+                                        <div className="relative px-5 flex-auto">
+                                            <p className="font-bold">รายละเอียด</p>
+                                            <p className="text-gray-900 text-sm leading-relaxed">
+                                                {/* {console.log(courseDetail)} */}
+                                                {courseDetail.subjectDescThai ? courseDetail.subjectDescThai : "ไม่มีข้อมูล"}
+                                            </p>
+                                        </div>
+                                        <div className="relative px-5 grid grid-cols-1 gap-2">
+                                            {console.log(sectionDate)}
+                                            <p className="font-bold">ตอน</p>
+                                            {courseSection ? courseSection.map((section, key) => <SectionCard key={key} data={section} />) : "ไม่มีข้อมูล"}
+                                        </div>
+                                    </div>
+                                    <div className="flex items-center justify-end p-5 border-t border-solid border-blueGray-200 rounded-b">
+                                        <button
+                                            className=" text-red-500 rounded-lg font-bold uppercase text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                            type="button"
+                                            onClick={handleCardClick}
+                                        >
+                                            Close
+                                        </button>
+                                    </div>
+                                </motion.div>
                             </div>
                         </div>
-                    </div>
-                    <div className="opacity-25 fixed inset-0 z-[100] bg-black"></div>
-                </>
-            )}
+                        <div className="opacity-25 fixed inset-0 z-[100] bg-black"></div>
+                    </>
+                )}
+            </AnimatePresence>
         </>
     )
 }
