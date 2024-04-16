6612c4635605573739cc8caaac26083874ca9f17
witayaporn <6410110483@email.psu.ac.th>
2024-04-17 00:34:30 +0700

    fix find_student to display all

--- back/server/routes_student.py
+++ back/server/routes_student.py
@@ -12,9 +12,9 @@ def list_student(request: Request):
     student = list(request.app.database["Student"].find())
     return student
 
-@router_student.get("/{studentId}", response_description="Get a single student by id", response_model=Student)
+@router_student.get("/{studentId}", response_description="Get a single student by id", response_model=List[Student])
 def find_student(studentId: int, request: Request):
-    if (student := request.app.database["Student"].find_one({"studentId": studentId})) is not None:
+    if (student := list(request.app.database["Student"].find({"studentId": studentId}))) is not None:
         return student
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Student with ID {studentId} not found")
 
