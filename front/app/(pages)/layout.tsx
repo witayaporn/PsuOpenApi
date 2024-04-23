cac3fda8b814d159769e3be0936b56740b83cc77
screamman-ssh <autogran41@gmail.com>
2024-04-23 22:33:12 +0700

    new header and adjust layout

--- front/app/(pages)/layout.tsx
+++ front/app/(pages)/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="main-font">
         <HeadNavbar/>
-        <div className="py-20 px-7 md:px-20 lg:px-40 xl:px-96">
+        <div className="pt-24 px-6 md:px-20 lg:px-40 xl:px-80">
           {children}
         </div>
         <BottomNavbar/>
