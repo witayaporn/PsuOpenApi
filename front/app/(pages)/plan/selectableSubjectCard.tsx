2f1a19e8d7c92f00d7e28fe5c773b9bb0282e830
screamman-ssh <autogran41@gmail.com>
2024-04-25 00:52:03 +0700

    Set up selectable subject card in plan and delete not use image

--- front/app/(pages)/plan/selectableSubjectCard.tsx
+++ front/app/(pages)/plan/selectableSubjectCard.tsx
@@ -1,22 +1,25 @@
 "use client"
 import { useState } from "react"
 
-export default function SelectableSectionCard(prop: any, key: number) {
+export default function SelectableSectionCard(prop: any) {
     const [select, setSelect] = useState(false)
     const data = prop.data[0]
     // const noInterest: number = 143
     // const percentage: number = (data.noOffer / noInterest) * 100
     const subjectShortNameEN = data.subjectCode + " " + data.shortNameEng
-    const handleCardClick = () => { }
+    const handleCardClick = () => {
+        prop.onClick(data.subjectId, !select)
+        setSelect(!select)
+    }
 
 
     return (
-        <a
-            className={"p-4 border rounded-lg showdow-sm hover:shadow-md cursor-pointer transition-all " + (select ? "border-4 border-blue-300" : "")}
-            // style={{ background: `linear-gradient(to bottom, ${facColor.primary}, ${facColor.secondary})`, }}
-            onClick={() => setSelect(!select)}
-        >
-            {/* <div className={ "h-full bg-white rounded-r-md"}> */}
+            <a
+                className={"p-4 border rounded-lg showdow-sm hover:shadow-md cursor-pointer transition-all " + (select ? "border-4 border-blue-300" : "")}
+                // style={{ background: `linear-gradient(to bottom, ${facColor.primary}, ${facColor.secondary})`, }}
+                onClick={handleCardClick}
+            >
+                {/* <div className={ "h-full bg-white rounded-r-md"}> */}
                 <p className="font-bold text-green-950">{subjectShortNameEN}</p>
                 <p className="text-gray-700 truncate max-h-6">{data.subjectNameThai}</p>
                 <p className="text-gray-700 truncate text-sm max-h-6">ตอน {data.section}</p>
@@ -24,7 +27,8 @@ export default function SelectableSectionCard(prop: any, key: number) {
                 <p className="inline-flex py-1 px-2 mx-2 text-black text-xs bg-gray-100 border-gray-400 rounded-lg">
                     {data.subjectTypeDesc}
                 </p>
-            {/* </div> */}
-        </a>
+                {/* </div> */}
+            </a>
+
     )
 }
