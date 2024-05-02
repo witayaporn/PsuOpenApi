32740c851095b7c8d433dd3a15dc03e4727b318d
screamman-ssh <autogran41@gmail.com>
2024-05-03 00:16:16 +0700

    Add room search button

--- front/app/(pages)/subjectSearch/sectionCard.tsx
+++ front/app/(pages)/subjectSearch/sectionCard.tsx
@@ -1,6 +1,6 @@
 
 const timeFormatter = (time: string) => {
-    if(time){
+    if (time) {
         const h = time.slice(0, 2)
         const m = time.slice(2, 4)
         return h + ":" + m + " น."
@@ -29,10 +29,10 @@ export default function SectionCard(prop: any) {
                     <p>จำนวนที่นั่ง</p>
                     <p>จำนวนคนที่สนใจ</p>
                 </div>
-                <div className="grid grid-cols-3 text-sm text-gray-600">
-                    <p>{data.studentGroup}</p>
-                    <p>{data.noOffer}</p>
-                    <p>{noInterest}</p>
+                <div className="grid grid-cols-3 text-sm break-words text-gray-600">
+                    <p className="p-1">{data.studentGroup}</p>
+                    <p className="p-1">{data.noOffer}</p>
+                    <p className="p-1">{noInterest}</p>
                 </div>
             </div>
             <div>
@@ -41,13 +41,28 @@ export default function SectionCard(prop: any) {
                     <p>ห้องเรียน</p>
                     <p>ผู้สอน</p>
                 </div>
-                <div className="grid grid-cols-3 gap-y-2 text-sm text-gray-600">
+                <div className="grid grid-cols-3 gap-y-2 text-sm break-normal text-gray-600">
                     {
-                        dateData ? dateData.map((date) =>
+                        dateData ? dateData.map((date: any) =>
                             <>
-                                <p>{date.classDateDesc} {timeFormatter(date.startTime) + " - " + timeFormatter(date.stopTime)}</p>
-                                <p>{date.roomName ? date.roomName : "-"}</p>
-                                <p>{date.lecturerNameThai}</p>
+                                <p className="p-1">{date.classDateDesc} {timeFormatter(date.startTime) + " - " + timeFormatter(date.stopTime)}</p>
+                                {date.roomName ?
+                                    <p className="block md:inline-flex p-1">
+                                        <p className="py-1 md:mr-3">{date.roomName}</p>
+                                        <a 
+                                            className="inline-flex py-1 px-2 bg-gray-300 w-fit h-fit rounded-lg" 
+                                            href={`/mapSearch/?search=${date.roomName}`}
+                                            target="_blank" rel="noopener noreferrer"
+                                        >
+                                            ค้นหาห้อง
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 ml-1">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                            </svg>
+
+                                        </a>
+                                    </p>
+                                    : <p>-</p>}
+                                <p className="p-1">{date.lecturerNameThai}</p>
                             </>
                         )
                             : "ไม่มีข้อมูล"
@@ -60,7 +75,7 @@ export default function SectionCard(prop: any) {
                     <p>สอบกลางภาค</p>
                     <p>สอบปลายภาค</p>
                 </div>
-                <div className="grid grid-cols-2 text-sm text-gray-600">
+                <div className="grid grid-cols-2 text-sm break-words text-gray-600">
                     {console.log(examData)}
                     {console.log(midExam)}
                     {midExam ? <p>{`${timeFormatter(midExam.examStartTime)} - ${timeFormatter(midExam.examStopTime)} ห้อง ${midExam.roomName ? midExam.roomName : "-"}`}</p> : <p>-</p>}
