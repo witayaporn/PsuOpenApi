5d34be3b313d9d2b83ef62e5a7ff72a1e021fea2
6410110483 <101465600+witayaporn@users.noreply.github.com>
2024-05-02 16:29:10 +0700

    add Get by year and term

--- back/server/routes_student.py
+++ back/server/routes_student.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Body, Request, Response, HTTPException, status # type: ignore
 from fastapi.encoders import jsonable_encoder # type: ignore
-from typing import List
+from typing import List, Optional
 from bson import ObjectId  # type: ignore
 
 from mongo.models import Student, StudentUpdate
@@ -55,3 +55,12 @@ def delete_student(studentId: str, request: Request, response: Response):
 
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with ID {studentId} not found")
 
+@router_student.get("/{year}", response_description="Get a student by year and term", response_model=List[Student])
+def find_student(year: int, term: Union[int, None] = None request: Request):
+    if (term != None):
+        if (student := list(request.app.database["Student"].find({"year": year, "term": term}))) is not None:
+            return student
+    else:
+        if (student := list(request.app.database["Student"].find({"year": year}))) is not None:
+            return student
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Student with ID {studentId} not found")
