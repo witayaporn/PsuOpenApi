06484dc0ea4fbd03b5df4955c15ce878e3ed0807
screamman-ssh <autogran41@gmail.com>
2024-05-12 21:26:51 +0700

    Add suspend component

--- front/app/(pages)/layout.tsx
+++ front/app/(pages)/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import BottomNavbar from "../components/bottomNavbar";
 import HeadNavbar from "../components/headNavbar";
 import { SessionProvider } from "next-auth/react";
+import { Suspense } from "react";
 
 export default function RootLayout({
     children,
@@ -15,7 +16,7 @@ export default function RootLayout({
                 <SessionProvider>
                     <HeadNavbar />
                     <div className="pt-28 px-6 md:px-20 lg:px-40 xl:px-80 mb-20 md:mb-0 ">
-                        {children}
+                        <Suspense>{children}</Suspense>
                     </div>
                     <BottomNavbar />
                 </SessionProvider>
