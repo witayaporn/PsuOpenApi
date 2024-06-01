a35093ec0a7a6ffc6b72be2e78042d4ad8a3cbe0
witayaporn <6410110483@email.psu.ac.th>
2024-06-01 23:22:48 +0700

    update api routes comment and add api route vote

--- back/server/routes_comment.py
+++ back/server/routes_comment.py
@@ -28,7 +28,7 @@ async def create_comment(request: Request, comment: Comment = Body(...)):
     return created_comment
 
 @router_comment.put("/{xSubjectId}", response_description="Update a Comment", response_model=Comment)
-async def update_comment(xSubjectId: int, request: Request, comment: CommentUpdate = Body(...)):
+async def update_comment(xSubjectId: str, request: Request, comment: CommentUpdate = Body(...)):
     comment = {k: v for k, v in comment.dict().items() if v is not None}
     if len(comment) >= 1:
         update_result = request.app.database["Comment"].update_one(
@@ -46,7 +46,7 @@ async def update_comment(xSubjectId: int, request: Request, comment: CommentUpda
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Student with ID {xSubjectId} not found")
 
 @router_comment.delete("/{xSubjectId}", response_description="Delete a Comment")
-async def delete_comment(xSubjectId: int, request: Request, response: Response):
+async def delete_comment(xSubjectId: str, request: Request, response: Response):
     delete_result = request.app.database["Comment"].delete_one({"xSubjectId": xSubjectId})
     
     if delete_result.deleted_count == 1:
