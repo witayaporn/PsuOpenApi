83da67971dded345316fbcdf131a67cb2b7b8649
screamman-ssh <autogran41@gmail.com>
2024-06-01 15:32:56 +0700

    Add nested comment box (dummy)

--- front/app/(pages)/subjectSearch/commentModal.tsx
+++ front/app/(pages)/subjectSearch/commentModal.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { Comment } from "./comment";
 
 export default function CommentModal(prop: any) {
     return (
@@ -6,7 +7,7 @@ export default function CommentModal(prop: any) {
             <div className="justify-center flex overflow-x-hidden overflow-y-scroll fixed inset-0 z-[10200] outline-none">
                 <div className="relative w-full md:w-2/4 m-auto">
                     <motion.div
-                        className="w-full h-fit grid grid-cols-1 gap-2 border-0 rounded-lg shadow-lg relative bg-white outline-none focus:outline-none"
+                        className="w-full h-fit grid grid-cols-1 gap-2 p-5 pr-0 border-0 rounded-lg shadow-lg relative bg-white outline-none focus:outline-none"
                         initial={{ opacity: 0, scale: 0.75 }}
                         animate={{
                             opacity: 1,
@@ -25,7 +26,7 @@ export default function CommentModal(prop: any) {
                             },
                         }}
                     >
-                        <div className="flex overflow-hidden p-6 md:pb-12 md:px-36 border-b border-solid rounded-lg">
+                        <div className="flex pr-5">
                             <button
                                 className="absolute top-1 right-0 px-2 text-gray-500 hover:bg-gray-300 hover:text-red-500 rounded-lg font-bold uppercase text-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                 type="button"
@@ -33,6 +34,12 @@ export default function CommentModal(prop: any) {
                             >
                                 X
                             </button>
+                            <p className="w-full font-bold text-lg border-b border-solid">ความคิดเห็นต่อรายวิชา</p>
+                        </div>
+                        <div className="flex flex-col max-h-screen md:max-h-[80vh] overflow-y-auto pr-3">
+                            <Comment data={{}}/>
+                            <Comment />
+                            <Comment />
                         </div>
                     </motion.div>
                 </div>
